5225f71366ff60911152d52efbe84afbed3d3939
German Berezhko <german.berezhko@skycase.ru>
2015-02-24 20:27:44 +0400

    Files.walk, try with resources

--- 3th/src/ru/ifmo/ctddev/berezhko/walk/RecursiveWalk.java
+++ 3th/src/ru/ifmo/ctddev/berezhko/walk/RecursiveWalk.java
@@ -1,21 +1,17 @@
 package ru.ifmo.ctddev.berezhko.walk;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RecursiveWalk {
-    private interface FileReadListener {
-        public void onGetFile(File file);
-    }
-
     private static int getFileHash(File file) {
         if (!file.exists()) {
             System.err.println("File or directory \"" + file.getPath() + "\" from input file not found");
             return 0;
         }
         int fileHash = 0;
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(file);
+        try (final FileInputStream fileInputStream = new FileInputStream(file)) {
             try {
                 int ch;
                 fileHash = 0x811c9dc5;
@@ -29,83 +25,53 @@ public class RecursiveWalk {
             }
         } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
-            return 0;
-        } finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return 0;
-                }
-            }
+        } catch (IOException e) {
+            System.err.println("IO error");
         }
         return fileHash;
     }
 
-    private static void recursiveWalk(File fileOrDirectory, FileReadListener listener) {
-        if (fileOrDirectory.isDirectory()) {
-            File[] files = fileOrDirectory.listFiles();
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    recursiveWalk(file, listener);
-                } else {
-                    listener.onGetFile(file);
-                }
-            }
-        } else {
-            listener.onGetFile(fileOrDirectory);
-        }
-    }
-
     public static void main(String args[]) {
-        Reader reader = null;
-        Writer writer = null;
-        BufferedReader bufferedReader;
-        try {
-            reader = new InputStreamReader(new FileInputStream(args[0]), "UTF-8");
-            writer = new OutputStreamWriter(new FileOutputStream(args[1]), "UTF-8");
-            bufferedReader = new BufferedReader(reader);
+        if (args == null || args.length < 2 || args[0] == null || args[1] == null) {
+            System.err.println("Input or output don't exist");
+            return;
+        }
+
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "UTF-8"));
+             final Writer writer = new PrintWriter(new OutputStreamWriter(new FileOutputStream(args[1]), "UTF-8"));
+        ) {
             boolean running = true;
             while (running) {
                 try {
-                    String dirOrFileName = bufferedReader.readLine();
+                    String dirOrFileName = reader.readLine();
                     if (dirOrFileName == null) {
                         running = false;
                         break;
                     }
-                    final File file = new File(dirOrFileName);
-
-                    final Writer finalWriter = writer;
-                    recursiveWalk(file, new FileReadListener() {
-                        @Override
-                        public void onGetFile(File file) {
-                            try {
-                                finalWriter.write(String.format("%08x", getFileHash(file)) + " " + file.getPath() + "\n");
-                            } catch (IOException e) {
-                            }
-                        }
-                    });
+                    if (!Files.exists(Paths.get(dirOrFileName))) {
+                        writer.write(String.format("%08x", 0) + " " + dirOrFileName + "\n");
+                    } else {
+                        Files.walk(Paths.get(dirOrFileName))
+                                .filter(Files::isRegularFile)
+                                .forEach(filePath -> {
+                                    try {
+                                        File file = filePath.toFile();
+                                        writer.write(String.format("%08x", getFileHash(file)) + " " + file.getPath() + "\n");
+                                    } catch (IOException e) {
+                                        System.err.println("Something wrong with writing in output");
+                                    }
+                                });
+                    }
                 } catch (IOException e) {
+                    System.err.println("Something wrong with reading from input");
                 }
             }
-        } catch (UnsupportedEncodingException e) {
-            // only UTF-8
         } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                }
-            }
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                }
-            }
+            System.err.println("Input file " + args[0] + " don't exist");
+        } catch (IOException e) {
+            System.err.println("IO error");
+        } catch (NullPointerException e) {
+            System.err.println("Type names of input and output files");
         }
     }
 }
