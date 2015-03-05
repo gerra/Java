70ddaad228ffdbef3628e7768d79dc0f06cc0710
gerra <gerralizza@gmail.com>
2015-03-05 03:34:02 +0300

    iterator

--- 3th/src/ru/ifmo/ctddev/berezhko/arrayset/ReversedArrayList.java
+++ 3th/src/ru/ifmo/ctddev/berezhko/arrayset/ReversedArrayList.java
@@ -1,6 +1,8 @@
 package ru.ifmo.ctddev.berezhko.arrayset;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Created by root on 05.03.15.
@@ -15,5 +17,25 @@ public class ReversedArrayList<E> extends ArrayList<E> {
     @Override
     public E get(int index) {
         return forwardList.get(forwardList.size() - index - 1);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int it = 0;
+
+            @Override
+            public boolean hasNext() {
+                return it != forwardList.size();
+            }
+
+            @Override
+            public E next() {
+                if (it >= forwardList.size()) {
+                    throw new NoSuchElementException();
+                }
+                return get(it++);
+            }
+        };
     }
 }
