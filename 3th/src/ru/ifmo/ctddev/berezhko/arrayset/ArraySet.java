6f3d5b93926106e207bbca3357f40c5926cd8de1
gerra <gerralizza@gmail.com>
2015-03-05 03:13:27 +0300

    ReversedArrayList added

--- 3th/src/ru/ifmo/ctddev/berezhko/arrayset/ArraySet.java
+++ 3th/src/ru/ifmo/ctddev/berezhko/arrayset/ArraySet.java
@@ -1,6 +1,8 @@
 package ru.ifmo.ctddev.berezhko.arrayset;
 
 
+import com.sun.istack.internal.Nullable;
+
 import java.util.*;
 
 /**
@@ -8,26 +10,23 @@ import java.util.*;
  */
 
 
-public class ArraySet<E> implements NavigableSet<E>  {
+public class ArraySet<E> extends AbstractSet<E>
+        implements NavigableSet<E>  {
 
     private List<E> set;
-    private Comparator<? super E> realComparator;
     private Comparator<? super E> comparator;
 
 
     public ArraySet(Collection<? extends E> set, Comparator<? super E> comparator) {
         this.set = new ArrayList<>(set);
-        this.realComparator = comparator;
-        this.comparator = comparator != null ? comparator : new Comparator<E>() {
-            @Override
-            public int compare(E o1, E o2) {
-                return ((Comparable)o1).compareTo(o2);
-            }
-        };
-        this.set.sort(comparator);
-        ArrayList<E> unique = new ArrayList<>();
+        this.comparator = comparator;
+
+        Collections.sort(this.set, comparator);
+        List<E> unique = new ArrayList<>();
         for (E e : this.set) {
-            if (unique.size() == 0 || comparator == null || comparator.compare(unique.get(unique.size() - 1), e) != 0) {
+            if (unique.size() == 0
+                    || comparator == null && ((Comparable)unique.get(unique.size() - 1)).compareTo(e) != 0
+                    || comparator.compare(unique.get(unique.size() - 1), e) != 0) {
                 unique.add(e);
             }
         }
@@ -48,76 +47,61 @@ public class ArraySet<E> implements NavigableSet<E>  {
 
     private ArraySet(List<E> set, Comparator<? super E> comparator) {
         this.set = set;
-        this.comparator = comparator != null ? comparator : new Comparator<E>() {
-            @Override
-            public int compare(E o1, E o2) {
-                return ((Comparable)o1).compareTo(o2);
-            }
-        };
+        this.comparator = comparator;
     }
 
-    // -1  true: <=
-    // -1 false:  <
-    //  1 false:  >
-    //  1  true: >=
-    private int binSearchInd(E x, int signType, boolean canNull) {
-        int l = 0;
-        int r = size();
-        int res = -1;
-        while (l != r) {
-            int m = (l + r) / 2;
-            int sign = comparator.compare(set.get(m), x);
+    // -1 false : lower
+    // -1 true : floor
+    // 1 true : ceiling
+    // 1 false : higher
+    public int binSearchInd(E e, int signType, boolean canEqual) {
+        boolean exists = true;
+        int ind = Collections.binarySearch(set, e, comparator);
+        if (ind < 0) {
+            exists = false;
+            ind = -ind - 1;
+        }
 
-            if (signType == -1) {
-                if (sign < 0 && !canNull || sign <= 0 && canNull) {
-                    res = m;
-                    l = m+1;
-                } else {
-                    r = m;
+        if (signType == -1) {
+            if (canEqual) {
+                if (!exists) {
+                    ind--;
                 }
             } else {
-                if (sign > 0 && !canNull || sign >= 0 && canNull) {
-                    res = m;
-                    r = m;
-                } else {
-                    l = m+1;
+                ind--;
+            }
+        } else {
+            if (!canEqual) {
+                if (exists) {
+                    ind++;
                 }
             }
         }
-        if (res == size()) {
-            res = -1;
-        }
-        return res;
-    }
-
-
-    public E binSearch(E x, int signType, boolean canNull) {
-        int ind = binSearchInd(x, signType, canNull);
-        if (ind == -1) {
-            return null;
-        } else {
-            return set.get(ind);
-        }
+        return ind;
     }
 
     @Override
     public E lower(E e) {
-        return binSearch(e, -1, false);
+        int ind = binSearchInd(e, -1, false);
+        return (ind >= 0 && ind < set.size() ? set.get(ind) : null);
     }
 
     @Override
     public E floor(E e) {
-        return binSearch(e, -1, true);
+        int ind = binSearchInd(e, -1, true);
+        return (ind >= 0 && ind < set.size() ? set.get(ind) : null);
     }
 
     @Override
     public E ceiling(E e) {
-        return binSearch(e, 1, true);
+        int ind = binSearchInd(e, 1, true);
+        return (ind >= 0 && ind < set.size() ? set.get(ind) : null);
     }
 
     @Override
     public E higher(E e) {
-        return binSearch(e, 1, false);
+        int ind = binSearchInd(e, 1, false);
+        return (ind >= 0 && ind < set.size() ? set.get(ind) : null);
     }
 
     @Override
@@ -126,61 +110,39 @@ public class ArraySet<E> implements NavigableSet<E>  {
     }
 
     @Override
-    public boolean isEmpty() {
-        return set.isEmpty();
-    }
-
-    @Override
     public boolean contains(Object o) {
         if (o == null) {
             return false;
         }
-        E ceiling = ceiling((E)o);
-        return (ceiling == null ? false : comparator.compare(ceiling, (E)o) == 0);
+        return Collections.binarySearch(set, (E)o, comparator) >= 0;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
-            private int currentIndex = 0;
+            Iterator<E> it = set.iterator();
 
             @Override
             public boolean hasNext() {
-                return currentIndex + 1 < size();
+                return it.hasNext();
             }
 
             @Override
             public E next() {
-                return set.get(currentIndex++);
+                return it.next();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
             }
         };
     }
 
     @Override
-    public Object[] toArray() {
-        return set.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return set.toArray(a);
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        for (Object e : c) {
-            if (!contains(e)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
     public NavigableSet<E> descendingSet() {
-        ArrayList<E> descendingSet = new ArrayList<>(set);
-        Collections.reverse(descendingSet);
-        return new ArraySet<>(descendingSet, realComparator);
+        List<E> descendingSet = new ReversedArrayList<>((ArrayList)set);
+        return new ArraySet<>(descendingSet, comparator != null ? comparator.reversed() : null);
     }
 
     @Override
@@ -191,22 +153,22 @@ public class ArraySet<E> implements NavigableSet<E>  {
     @Override
     public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
         if (size() == 0) {
-            return new ArraySet<>(realComparator);
+            return new ArraySet<>(comparator);
         }
 
         int from = binSearchInd(fromElement, 1, fromInclusive);
         int to = binSearchInd(toElement, -1, toInclusive);
 
         if (from == -1 || to == -1 || from > to) {
-            return new ArraySet<>(realComparator);
+            return new ArraySet<>(comparator);
         }
-        return new ArraySet<>(set.subList(from, to+1), realComparator);
+        return new ArraySet<>(set.subList(from, to + 1), comparator);
     }
 
     @Override
     public NavigableSet<E> headSet(E toElement, boolean inclusive) {
         if (size() == 0) {
-            return new ArraySet<>(realComparator);
+            return new ArraySet<>(comparator);
         }
         return subSet(first(), true, toElement, inclusive);
     }
@@ -214,14 +176,15 @@ public class ArraySet<E> implements NavigableSet<E>  {
     @Override
     public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
         if (size() == 0) {
-            return new ArraySet<>(realComparator);
+            return new ArraySet<>(comparator);
         }
         return subSet(fromElement, inclusive, last(), true);
     }
 
+    @Nullable
     @Override
     public Comparator<? super E> comparator() {
-        return realComparator;
+        return comparator;
     }
 
     @Override
@@ -263,36 +226,6 @@ public class ArraySet<E> implements NavigableSet<E>  {
 
     @Override
     public E pollLast() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean add(E e) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
         throw new UnsupportedOperationException();
     }
 }
